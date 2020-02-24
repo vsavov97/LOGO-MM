725292ae8a50651105ae51a034c49ac864bc2801
vsavov97 <61435914+vsavov97@users.noreply.github.com>
2020-02-24 23:33:04 +0200

    Add files via upload

---
+++ zadachka priem Vs.cpp
@@ -0,0 +1,42 @@
+// zadachka priem Vs.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "math.h"
+#include <iostream>
+
+using namespace std; 
+int main()
+{
+void print_logoMM(int n, int m);
+
+
+	int i,j ;
+	for (i = 1; n <= n; i++);
+	{
+		for (j = 1; j <= n; j++);
+	}
+	{
+	if (i == 1 || i == n || j == 1 || j == n || i == j || j == (n - i + 1));
+		cout << "*";
+		{
+	else
+		cout << " ";
+}
+cout << "\n";
+	}
+}
+}
+
+//Sustavete konzolno prilojenie koeto izchartava logoto MM po zadaden perimetur N pokazvasht debelinata na bukvite M v gorniq krai na logoto
+//-za otbelqzvane na svobodnoto prostranstvo izpozlvaite simvol "-" a za izgrajdane na bukvite "*" . 
+// OGRANICHENIQ : 2<N<10.000, N e vinagi nechetno chislo .
+//1. nqma ogranicheniq za izpolzvaniqt ezik "Az sum zapoznat samomalko  c++ i malko s  c#"
+//2.pisaneto na po lesno razbiraem i chetim kod shte se prieme za plus 
+//3.sustavqneto na po kopleksno reshenie izpozvaiki OOP i prilagane na drugi dobri praktiki shte se schita za plus
+//tova e uslovieto napisal sum go ako nshto se oburka i ne  stigni screeshota sus zadachkata  do tozzi koito shte mi pomogni 
+// Here is your assignment. Please upload the code for this project to GitHub, and post a link to your repository below not later than 24.02.2020. *//
+{
+	return 0;
+}
+
